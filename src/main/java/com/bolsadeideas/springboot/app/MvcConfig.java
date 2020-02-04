4f516a92d118fbfc36bd0577ea9c159bd13290f0
jdasilva80 <jdasilva1980@gmail.com>
2020-02-04 21:40:09 +0100

    vistas xml

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,6 +12,8 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import com.bolsadeideas.springboot.app.view.xml.ClienteList;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -63,6 +66,17 @@ public class MvcConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 
 		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+	@Bean
+	public Jaxb2Marshaller jaxb2Marshaller() {
+
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+
+		marshaller.setClassesToBeBound(new Class[] { com.bolsadeideas.springboot.app.view.xml.ClienteList.class });
+
+		System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ hola" +marshaller.getContextPath());
+		return marshaller;
 	}
 
 }
